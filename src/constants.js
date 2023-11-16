d70fb6a96980333e787d65f5f45b4b454b97073b
Shakir <shakirrahman81@gmail.com>
2023-11-16 11:27:28 -0500

    Edits emojis used.

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 module.exports.LIKES = ":sparkling_heart:";
-module.exports.RETWEETS = ":repeat:";
+module.exports.RETWEETS = ":recycle:";
 module.exports.REPLIES = ":speech_balloon:";
 module.exports.API = "https://api.vxtwitter.com/Twitter/status/";
 module.exports.TWITTER = "https://twitter.com/";
